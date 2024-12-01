92d5c77051fae0ba35441150c023b697317b9696
Axel Sundin <awite_sundin@hotmail.com>
2024-12-01 10:09:35 +0100

    day01 refactor

--- day01.py
+++ day01.py
@@ -5,25 +5,14 @@ with open('input01.txt', 'r') as puzzleInput:
 		a = line.split('   ')
 		list1.append(int(a[0]))
 		list2.append(int(a[1]))
+	
+	# PART 1
 	list1.sort()
 	list2.sort()
-
-	tot = 0
-	for i in range(len(list1)):
-		tot += abs(list1[i] - list2[i])
-
-	print(f"Part 1: {tot}")
+	dif = sum([abs(a - b) for a, b in zip(list1, list2)])
+	print(f'Part 1: {dif}')
 
 	# PART 2
-	dict = {}
-	for e in list1:
-		dict[e] = 0
-
-	for e in list2:
-		if e in dict:
-			dict[e] += 1
-	
-	sum = 0
-	for e in dict:
-		sum += int(e) * dict[e]
-	print(f"Part 2: {sum}")
+	occurances = {e: list2.count(e) for e in list1}
+	similarityScore = sum(e * count for e, count in occurances.items())
+	print(f'Part 2: {similarityScore}')
